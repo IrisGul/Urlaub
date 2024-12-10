381bdf0780335b5ded4bef9d3c30cd123d6a9383
Yrysgul Sydykbekova <yrysgul.sydykbekova@gmail.com>
2024-12-10 20:54:31 +0100

    ort abkürzungen substring, initialen, pos, indexOf

--- Urlaub.java
+++ Urlaub.java
@@ -40,6 +40,31 @@ public class Urlaub
     {
         return allInclusive;
     }
+    public String getZielort()
+    {
+        int pos;
+        String zielOrt;
+        pos=ort.indexOf("-"); //5
+        zielOrt=ort.substring(0,pos);
+        return zielOrt;   
+    }
+    public String getLand()
+    {
+        int pos;
+        String land;
+        pos=ort.indexOf("-")+1;
+        land=ort.substring(pos);
+        return land;
+    }
+    public String getInitialen()
+    {
+        String initialen;
+        int pos;
+        initialen=ort.substring(0,1);
+        pos=ort.indexOf("")+1;
+        initialen=initialen + ort.substring(pos,pos+1);
+        return initialen;
+    }
     public void setOrt(String ort)
     {
         this.ort=ort;
